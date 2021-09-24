177d28c68da48d0242df16a95e0a689054ae7950
Frank Tran <frank.tran@live.ca>
2021-09-24 18:06:08 -0400

    Adding Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './containers/App';
 import './containers/index.css';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
